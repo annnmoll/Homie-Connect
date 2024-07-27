2325cbfad098412880fd9267f6631ee324483c4a
Abhishek-Ahuja <anmolahuja250@gmail.com>
2024-07-27 19:35:07 +0530

    Good to go

--- flatmate/src/services/apis.js
+++ flatmate/src/services/apis.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:4000"
-// const BASE_URL = "https://homie-connect.vercel.app"
+// const BASE_URL = "http://localhost:4000"
+const BASE_URL = "https://homie-connect.vercel.app"
 
 export const apis = {
     SIGNUP : BASE_URL + `/register`,
