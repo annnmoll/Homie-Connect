b6dbd9b12d378d62e9ae8c23cbe81ccd7862e6a2
Abhishek-Ahuja <anmolahuja250@gmail.com>
2024-11-08 13:14:46 +0530

    testing

--- server/utils/imageUploader.js
+++ server/utils/imageUploader.js
@@ -1,11 +1,10 @@
-const cloudinary = require("cloudinary").v2 ; 
+const cloudinary = require("cloudinary").v2;
 
-exports.uploadImage = async(image , folder)=>{
-  try{
-    const response = await  cloudinary.uploader.upload(image, 
-    { folder });
-    return response ; 
-  }catch(error){
-    return error ;
+exports.uploadImage = async (image, folder) => {
+  try {
+    const response = await cloudinary.uploader.upload(image, { folder });
+    return response;
+  } catch (error) {
+    return error;
   }
-}
+};
